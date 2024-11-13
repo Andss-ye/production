ba7f69d60ef57e2eb45fbd39b75bfc5349543242
Andss-ye <andrex120tv@gmail.com>
2024-11-13 07:02:33 -0500

    feat: :rocket: Se agrego la funcion principal del menu en main para el funcionamiento basico

---
+++ main.py
@@ -0,0 +1,34 @@
+def menu():
+    print('\n======== BIENVENIDO AL SISTEMA DE GESTIÓN DE PRODUCTOS ========')
+    print('1. Registrar producto')
+    print('2. Registrar materia prima')
+    print('3. Listar productos')
+    print('4. Listar materias primas')
+    print('5. Salir del programa')
+    opc = int(input('\nSeleccione su opcion: '))
+    return opc
+
+opc = 10
+
+while opc != 0:
+    opc = menu()
+
+    if opc == 1:
+        print('registrar producto')
+
+    elif opc == 2:
+        print('registrar materia prima')
+
+    elif opc == 3:
+        print('listar productos')
+    
+    elif opc == 4:
+        print('listar materias primas')
+
+    elif opc == 0:
+        print('Saliendo del programa...')
+        exit()
+    
+    else:
+        print('\nOpcion no valida, ingrese otra')
+        
